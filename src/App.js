30eb75a7738aef6d9d46a651594a10069d5981c0
nawazsk <nawaz.540@gmail.com>
2020-05-24 12:35:22 +0530

    create react app demo

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Create React App Demo
         </a>
       </header>
     </div>
